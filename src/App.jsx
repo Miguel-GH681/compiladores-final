da7c463cc71cd285c3bf6b154c3249812ef3bf29
miguelgh <miguelgh681@gmail.com>
2026-05-26 13:08:44 -0600

    Revision de UI matriz recursiva

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import logo from "./assets/icon_cosmos.png"
 
 function App() {
 
   const [text, setText] = useState("");
-
   const [variables, setVariables] = useState([]);
-
   const [terminales, setTerminales] = useState([]);
-
   const [producciones, setProducciones] = useState([]);
+  const [gramaticaSinRecursion, setGramaticaSinRecursion] = useState([]);
+  const [variablesSinRecursion, setVariablesSinRecursion] = useState([]);
 
   const analizarTexto = () => {
 
     const variablesEncontradas = [];
-
     const regexVariables = /([A-Za-z_][A-Za-z0-9_]*)\s*::/g;
 
     let matchVariable;
@@ -30,8 +29,7 @@ function App() {
 
 
     const terminalesEncontradas = [];
-
-    const regexTerminales = /’([^’]+)’/g;
+    const regexTerminales = /'([^']+)'/g;
 
     let matchTerminal;
 
@@ -59,7 +57,7 @@ function App() {
         const listaProducciones =produccionesTexto.split("|");
 
         listaProducciones.forEach((produccion) => {
-          const produccionLimpia = produccion.replaceAll("’", " ").trim();
+          const produccionLimpia = produccion.replaceAll("'", " ").trim();
 
           produccionesEncontradas.push({
             variable: variable,
@@ -73,6 +71,104 @@ function App() {
     setVariables(variablesEncontradas);
     setTerminales(terminalesEncontradas);
     setProducciones(produccionesEncontradas);
+    eliminarRecursividadIzquierda(produccionesEncontradas);
+  };
+
+  const eliminarRecursividadIzquierda = (listaProd) => {
+    /*
+      Agrupar producciones por variable
+    */
+    const agrupadas = {};
+
+    listaProd.forEach((item) => {
+      if (!agrupadas[item.variable]) {
+        agrupadas[item.variable] = [];
+      }
+      agrupadas[item.variable].push(item.produccion);
+    });
+
+    const resultado = [];
+    const listaDeVariables = [];
+    /*
+      Analizar variable por variable
+    */
+    Object.keys(agrupadas).forEach((variable) => {
+      const listaProducciones = agrupadas[variable];
+      const recursivas = [];
+      const noRecursivas = [];
+
+      listaProducciones.forEach((prod) => {
+
+        const simbolos = prod.split(" ");
+
+        /*
+          ¿Inicia con la misma variable?
+        */
+        if (simbolos[0] === variable) {
+          /*
+            Guardar α
+          */
+          const resto = simbolos.slice(1).join(" ");
+          recursivas.push(resto);
+        } else {
+          noRecursivas.push(prod);
+        }
+      });
+
+      /*
+        Si NO tiene recursividad
+      */
+      if (recursivas.length === 0) {
+        listaProducciones.forEach((prod) => {
+          resultado.push({
+            variable,
+            produccion: prod,
+          });
+        });
+      } else {
+
+        /*
+          Crear nueva variable
+        */
+        const nuevaVariable = `${variable}'`;
+        /*
+          A :: β A'
+        */
+        noRecursivas.forEach((beta) => {
+          resultado.push({
+            variable,
+            produccion: `${beta} ${nuevaVariable}`,
+          });
+        });
+
+        /*
+          A' :: α A'
+        */
+        recursivas.forEach((alpha) => {
+          resultado.push({
+            variable: nuevaVariable,
+            produccion: `${alpha} ${nuevaVariable}`,
+          });
+        });
+
+        /*
+          A' :: ε
+        */
+        resultado.push({
+          variable: nuevaVariable,
+          produccion: "ε",
+        });
+      }
+    });
+    
+    resultado.forEach(gsr =>{
+      if(!listaDeVariables.includes(gsr.variable)){
+        listaDeVariables.push(gsr.variable)
+      }
+    })
+
+    setGramaticaSinRecursion(resultado);
+    setVariablesSinRecursion(listaDeVariables);
   };
 
   useEffect(() => {
@@ -96,89 +192,142 @@ function App() {
   }, [text]);
 
   return (
-    <div className="container">
-
-      <div className="left-column">
-
-        <div className="editor-header">
-
-          <h2>Entrada (editor de texto)</h2>
-
-          <button
-            className="action-button"
-            onClick={analizarTexto}
-          >
-            F5
-          </button>
-
+    <div>
+      <div className="header">
+        <div className="icon-box">
+          <img src={logo} alt="logo" />
+          <h1>Cosmos</h1>
         </div>
+        <div>
+          <h3>Alvaro Miguel González Hic 9490-22-4805</h3>
+        </div>
+      </div>
+      <hr />
+      <div className="container">
+        <div className="left-column">
 
-        <textarea
-          className="text-editor"
-          placeholder="Escribe tu gramática aquí..."
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
+          <div className="editor-header">
 
-        <div className="bottom-row">
+            <h2>Entrada (editor de texto)</h2>
 
-          <div className="bottom-left">
-
-            <div className="table-container">
-              <table>
-                <thead>
-                  <tr>
-                    <th>Variable</th>
-                  </tr>
-                </thead>
-
-                <tbody>
-
-                  {
-                    variables.map((variable, index) => (
-                      <tr>
-                        <td>{variable}</td>
-                      </tr>
-                    ))
-                  }
-
-                </tbody>
-
-              </table>
-
-            </div>
-
-            <div className="table-container">
-              <table>
-
-                <thead>
-                  <tr>
-                    <th>Terminal</th>
-                  </tr>
-                </thead>
-
-                <tbody>
-
-                  {
-                    terminales.map((terminal, index) => (
-                      <tr>
-                        <td>{terminal}</td>
-                      </tr>
-                    ))
-                  }
-
-                </tbody>
-
-              </table>
-
-            </div>
+            <button
+              className="action-button"
+              onClick={analizarTexto}
+              title="Presiona para ejecutar el programa"
+            >
+              Ejecutar
+            </button>
 
           </div>
 
-         <div className="bottom-right">
-          {/* <div className="table-header">
-            Matriz de producciones
-          </div> */}
+          <textarea
+            className="text-editor"
+            placeholder="Escribe tu gramática aquí..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+
+
+          <div className="bottom-row">
+            <div className="bottom-left-container">
+              <div className="bottom-left-container-title">
+                <p><b>Vectores</b></p>
+                <hr />
+              </div>
+              <div className="bottom-left">
+                <div className="table-container">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th><b>V</b></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        variables.map((variable, index) => (
+                          <tr key={index}>
+                            <td>{variable}</td>
+                          </tr>
+                        ))
+                      }
+                    </tbody>
+                  </table>
+                </div>
+
+                <div className="table-container">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th><b>T</b></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        terminales.map((terminal, index) => (
+                          <tr key={index}>
+                            <td>{terminal}</td>
+                          </tr>
+                        ))
+                      }
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+
+            <div className="bottom-right-container">
+              <div className="bottom-right-container-title">
+                <p><b>Matriz de producciones</b></p>
+                <hr />
+              </div>
+              <div className="bottom-right">
+                <div className="table-container">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th><b>V</b></th>
+                        <th><b>Producciones</b></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        producciones.map((item, index) => (
+
+                          <tr key={index}>
+
+                            <td>{item.variable}</td>
+
+                            <td>{item.produccion}</td>
+
+                          </tr>
+
+                        ))
+                      }
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right-column">
+          <div className="editor-gramatical">
+            <h2>Gramática sin Recursividad por la izquierda</h2>
+          </div>
+          <textarea
+            className="text-editor"
+            placeholder=""
+            value={            
+              variablesSinRecursion.map(vsr =>{
+                const produccionesXVariable = gramaticaSinRecursion.filter(gsr => gsr.variable == vsr);
+                
+                const texto = produccionesXVariable.map(pxv => pxv.produccion).join(" | ");
+              
+                return `${vsr} :: ${texto}`
+              }).join("\n")
+            }
+
+          />
 
           <div className="table-container">
             <table>
@@ -188,34 +337,22 @@ function App() {
                   <th>Producciones</th>
                 </tr>
               </thead>
+              
               <tbody>
                 {
-                  producciones.map((item, index) => (
-
-                    <tr key={index}>
-
-                      <td>{item.variable}</td>
-
-                      <td>{item.produccion}</td>
-
-                    </tr>
-
-                  ))
+                  gramaticaSinRecursion.map(
+                    (item, index) => (
+                      <tr key={index}>
+                        <td>{item.variable}</td>
+                        <td>{item.produccion}</td>
+                      </tr>
+                    )
+                  )
                 }
               </tbody>
             </table>
           </div>
         </div>
-        </div>
-      </div>
-      <div className="right-column">
-        <div className="editor-gramatical">
-          <h2>Gramática sin Recursividad por la izquierda</h2>
-        </div>
-        <textarea
-          className="text-editor"
-          placeholder=""
-        />
       </div>
     </div>
   );
